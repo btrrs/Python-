c967ba4b3f0635a65ca77f1a8aa46965c29d405d
btrrs <BRUNASTORRES24@GMAIL.COM>
2024-12-05 09:48:20 -0300

    Arquivo aula 02 py

---
+++ a u l a 2/aula02.py
@@ -0,0 +1,12 @@
+print('oioi, tudo bem?')
+
+nome_usuario = 'bruna torres' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.69 # tipo -> float = ponto flutuante 
+maior_idade = True  # tipo -> bool = boolean
+
+print ('seja bem-vindo', nome_usuario)
+print ('sua idade é', idade_usuario, 'anos')
+print ('voce tem', altura_usuario, 'metros')
+print ('você é maior de idade?', maior_idade)
+
